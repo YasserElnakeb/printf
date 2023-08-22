687e212c7da4b0437f004403f1bb1a3d7acac4e3
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-08-22 15:03:03 -0700

    upload function that handle printing integars

---
+++ print_integer_d.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_integer_d - prints integer
+ * @n : integer to print
+ * Return: number of characters printed
+ */
+int print_integer_d(int n)
+{
+int num, digit, unit_digit = n % 10, divisor = 1, _return = 1;
+n = n / 10;
+num = n;
+if (unit_digit < 0)
+{
+_return += _putchar('-');
+num = -num;
+n = -n;
+unit_digit = -unit_digit;
+}
+if (num > 0)
+{
+while (num / 10 != 0)
+{
+divisor *= 10;
+num /= 10;
+}
+num = n;
+while (divisor > 0)
+{
+digit = num / divisor;
+_return += _putchar(digit + '0');
+num -= (digit *divisor);
+divisor /= 10;
+}
+}
+_putchar(unit_digit + '0');
+return (_return);
+}
